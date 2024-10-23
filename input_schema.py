adc462786223b2074a61af41a11953f7a1482237
Nirmal2000 <nirmalvelu2000@gmail.com>
2024-10-23 22:39:32 +0530

    test

--- input_schema.py
+++ input_schema.py
@@ -9,12 +9,6 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["https://github.com/inferless/ComfyUI-Inferless-template/raw/main/workflows/sd1-5_workflow.json"]
-    },
-    "negative_prompt": {
-        'datatype': 'STRING',
-        'required': False,
-        'shape': [1],
-        'example': ["blurry, illustration, toy, clay, low quality, flag, nasa, mission patch"]
-    }
+        'example': ["https://github.com/Nirmal2000/ComfyUI-Inferless-template/raw/main/workflows/flux_workflow.json"]
+    },    
 }
